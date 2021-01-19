76c4eb1bec89b082995888987e376bb55b444b4c
ducktrshessami <jwee761@gmail.com>
2021-01-19 14:11:52 -0800

    handle ready and disconnect events

--- bot/index.js
+++ bot/index.js
@@ -2,8 +2,21 @@ const DiscordBot = require("discord-bot");
 const config = require("../config/discord.json");
 const commands = require("./commands");
 
+var client;
+
 require("dotenv").config();
 config.token = process.env.BOT_TOKEN;
 config.admin.push(process.env.BOT_DEVID);
 
-module.exports = new DiscordBot(config, commands);
+client = new DiscordBot(config, commands);
+
+client.on("ready", function() {
+    console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`);
+});
+
+client.on("shardDisconnect", function() {
+    console.log("Logged off");
+    process.exit();
+});
+
+module.exports = client;
