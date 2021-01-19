0b6042b369d266d12d66b406ff1fb4510247f564
ducktrshessami <jwee761@gmail.com>
2021-01-19 11:19:12 -0800

    unit name not null

--- models/unit.js
+++ models/unit.js
@@ -15,7 +15,10 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   unit.init({
-    name: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'unit',
