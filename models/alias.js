6b140d2c221daf6dd09679781c7e97b1e60a9c7e
ducktrshessami <jwee761@gmail.com>
2021-01-19 11:28:29 -0800

    associate alias with unit

--- models/alias.js
+++ models/alias.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      models.alias.belongsTo(models.unit, {
+        onDelete: "set null",
+        foreignKey: {
+          allowNull: false
+        }
+      });
     }
   };
   alias.init({
