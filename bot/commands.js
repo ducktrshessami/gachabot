f3d14b84dafca3556166a0c776cafc4ea9f25ad0
ducktrshessami <jwee761@gmail.com>
2021-01-19 17:43:51 -0800

    comment restart callback

--- bot/commands.js
+++ bot/commands.js
@@ -2,6 +2,9 @@ const { Command } = require("discord-bot");
 const utils = require("./utils");
 const db = require("../models");
 
+/*
+Restart Command callback
+*/
 function restart(message) {
     utils.sendVerbose(message.channel, "Restarting")
         .then(() => this.client.destroy())
