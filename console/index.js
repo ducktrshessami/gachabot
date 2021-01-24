6e1be6897b86ab643630f0cdc0740f03505ac766
ducktrshessami <jwee761@gmail.com>
2021-01-23 18:02:52 -0800

    create input stream for console

---
+++ console/index.js
@@ -0,0 +1,10 @@
+const { createInterface } = require("readline");
+const db = require("../models");
+
+var is = createInterface({ input: process.stdin });
+
+is.on("line", function(line) {
+
+});
+
+module.exports = is;
