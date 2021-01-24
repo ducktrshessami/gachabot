05c980a59fd26fac34674a956e6986318aace907
ducktrshessami <jwee761@gmail.com>
2021-01-23 18:18:18 -0800

    input command engine

--- console/index.js
+++ console/index.js
@@ -1,10 +1,30 @@
+const fs = require("fs");
+const path = require("path");
 const { createInterface } = require("readline");
-const db = require("../models");
 
 var is = createInterface({ input: process.stdin });
+var commands = {};
 
-is.on("line", function(line) {
+// Handle every other file in this dir as a command
+fs
+    .readdirSync(__dirname)
+    .filter(file => (
+        (file.indexOf('.') !== 0) &&
+        (file !== path.basename(__filename)) &&
+        (file.slice(-3) === '.js')
+    ))
+    .map(file => file.slice(0, -3).toLowerCase())
+    .forEach(file => commands[file] = require(path.join(__dirname, file)));
 
+// Handle input
+is.on("line", line => {
+    let cmd = line.trim().toLowerCase();
+    if (commands[cmd]) {
+        commands[cmd]();
+    }
+    else {
+        console.log(`'${line.trim()}' not recognized`);
+    }
 });
 
 module.exports = is;
