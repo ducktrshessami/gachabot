d52da07e7874fe969d2484ea26a2a29e9e39dac8
ducktrshessami <jwee761@gmail.com>
2021-01-24 18:53:37 -0800

    seeder filepath generation

--- db/backup.js
+++ db/backup.js
@@ -1,3 +1,28 @@
-module.exports = function() {
+const path = require("path");
+const db = require("../models");
 
+// Numbers, but fixed to 2 digits
+function extendo(n) {
+    return n < 10 ? `0${n}` : `${n}`;
+}
+
+// Generate a seeder filename
+function filepath() {
+    let date = new Date();
+    let extend = [
+        date.getUTCMonth() + 1,
+        date.getUTCDate(),
+        date.getUTCHours() + 1,
+        date.getUTCMinutes(),
+        date.getUTCSeconds()
+    ].map(extendo);
+    return path.join(__dirname, "..", "seeders", `${date.getUTCFullYear()}${extend.join("")}-backup.js`);
+}
+
+module.exports = function() {
+    for (let model in Object.keys(db)) {
+        if (model.toLowerCase() != "sequelize") {
+            
+        }
+    }
 };
