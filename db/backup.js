263c3d67ed60c503cf1e50461cec1a6446e6085c
ducktrshessami <jwee761@gmail.com>
2021-01-25 13:53:18 -0800

    async seed content generation

--- db/backup.js
+++ db/backup.js
@@ -1,5 +1,8 @@
 const path = require("path");
+const fs = require("fs");
 const db = require("../models");
+const { rejects } = require("assert");
+const { table } = require("console");
 
 // Numbers, but fixed to 2 digits
 function extendo(n) {
@@ -19,10 +22,42 @@ function filepath() {
     return path.join(__dirname, "..", "seeders", `${date.getUTCFullYear()}${extend.join("")}-backup.js`);
 }
 
-module.exports = function() {
-    for (let model in Object.keys(db)) {
-        if (model.toLowerCase() != "sequelize") {
-            
+// Outer seeder template
+function seedWrap(up, down) {
+    return `'use strict';
+
+    module.exports = {
+        up: async (queryInterface, Sequelize) => {
+            ${up}
+        },
+    
+        down: async (queryInterface, Sequelize) => {
+            ${down}
         }
-    }
+    };`;
+}
+
+//
+function seedUp(model, data) {
+    return `await queryInterface.bulkInsert("${model}", ${data});`;
+}
+
+function seedDown(model) {
+    return `await queryInterface.bulkDelete("${model}");`;
+}
+
+module.exports = function() {
+    return new Promise((resolve, reject) => {
+        let up = [], down = [];
+        Promise.all(Object.keys(db)
+            .filter(key => key.toLowerCase() != "sequelize")
+            .map(model => db[model].findAll()
+                .then(table => JSON.stringify(table, null, 4))
+                .then(tableData => up.push(seedUp(model, tableData)))
+                .then(() => down.push(seedDown(model)))
+            )
+        )
+            .then(() => seedWrap(up.join("\n"), down.join("\n")))
+            .then(console.log);
+    });
 };
