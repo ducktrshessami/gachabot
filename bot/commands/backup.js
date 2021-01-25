42cf46008f4b6ae9cb848496f37c9f4d83a5d182
ducktrshessami <jwee761@gmail.com>
2021-01-24 17:56:01 -0800

    backup admin command calls db util

--- bot/commands/backup.js
+++ bot/commands/backup.js
@@ -1,1 +1,9 @@
 const { Command } = require("discord-bot");
+const { sendVerbose } = require("../utils");
+const backup = require("../../db/backup");
+
+module.exports = new Command("backup", function(message) {
+    sendVerbose(message.channel, "Backing up the database").catch(console.error);
+    backup();
+    sendVerbose(message.channel, `${message.author} Finished backup`).catch(console.error);
+}, { admin: true });
