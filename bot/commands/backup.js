28781daa1ab168ccad3b19eb142ac8deeed9e6a4
ducktrshessami <jwee761@gmail.com>
2021-01-25 14:05:35 -0800

    handle backup bot command async

--- bot/commands/backup.js
+++ bot/commands/backup.js
@@ -4,6 +4,10 @@ const backup = require("../../db/backup");
 
 module.exports = new Command("backup", function(message) {
     sendVerbose(message.channel, "Backing up the database").catch(console.error);
-    backup();
-    sendVerbose(message.channel, `${message.author} Finished backup`).catch(console.error);
+    backup()
+        .then(() => sendVerbose(message.channel, `${message.author} Finished backup`))
+        .catch(err => {
+            console.log(err);
+            sendVerbose(message.channel, `${message.author} Encountered an error backing up the database`).catch(console.error);
+        });
 }, { admin: true });
