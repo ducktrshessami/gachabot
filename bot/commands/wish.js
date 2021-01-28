ce3fdcc2e74b066a84e2cd9d57964bc83650a175
ducktrshessami <jwee761@gmail.com>
2021-01-27 18:21:08 -0800

    create wish command

---
+++ bot/commands/wish.js
@@ -0,0 +1,7 @@
+const { Command } = require("discord-bot");
+const { logMessage } = require("../utils");
+const db = require("../../models");
+
+module.exports = new Command("wish", function(message, args) {
+
+});
