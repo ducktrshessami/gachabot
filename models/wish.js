c52c4571e13986f9730d27a69a044b883057d84e
ducktrshessami <jwee761@gmail.com>
2021-01-27 18:07:27 -0800

    wish same associations as claim

--- models/wish.js
+++ models/wish.js
@@ -10,7 +10,21 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      models.wish.belongsTo(models.guild, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+      models.wish.belongsTo(models.player, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+      models.wish.belongsTo(models.unit, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
     }
   };
   wish.init({}, {
