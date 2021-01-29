9e430f98416c59aff827e28b132b6d1599c04589
ducktrshessami <jwee761@gmail.com>
2021-01-28 17:10:33 -0800

    Revert "remove claim and wish models"
    This reverts commit 66637799338604fa13b60ca1c43780a3643e5fa0.

---
+++ models/wish.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class wish extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      models.wish.belongsTo(models.guild, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+      models.wish.belongsTo(models.player, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+      models.wish.belongsTo(models.unit, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    }
+  };
+  wish.init({}, {
+    sequelize,
+    modelName: 'wish',
+  });
+  return wish;
+};
