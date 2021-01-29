a6aa62e75186ae5506a5fa47d6f71b375b5a7dbe
ducktrshessami <jwee761@gmail.com>
2021-01-29 10:14:22 -0800

    handle not found info command

--- bot/commands/info.js
+++ bot/commands/info.js
@@ -3,17 +3,21 @@ const utils = require("../utils");
 const db = require("../../models");
 
 module.exports = new Command("info", function(message, args) {
-    let query = args.slice(1).join(" ").trim().toLowerCase();
+    let query = args.slice(1).join(" ").trim();
+    let queryLower = query.toLowerCase();
     utils.logMessage(message);
     db.alias.findOne({
         where: {
-            $and: db.Sequelize.where(db.Sequelize.fn("lower", db.Sequelize.col("name")), query)
+            $and: db.Sequelize.where(db.Sequelize.fn("lower", db.Sequelize.col("name")), queryLower)
         },
         include: {
             model: db.unit,
             include: db.image
         }
     })
-        .then(data => utils.sendVerbose(message.channel, "", utils.unitEmbed(data.name, data.unit.type, data.unit.images[0].url)))
+        .then(data => data ? ["", utils.unitEmbed(data.name, data.unit.type, data.unit.images[0].url)] : [`Could not find \`${query}\``])
+        .then(response => utils.sendVerbose(message.channel, ...response))
         .catch(console.error);
-}, { aliases: ["i"] });
+}, {
+    aliases: ["i"]
+});
