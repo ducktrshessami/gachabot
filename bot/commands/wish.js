d048987d5e0b4194c0f213a4af563511223c9d88
ducktrshessami <jwee761@gmail.com>
2021-01-28 16:47:24 -0800

    wish command returns the unit test

--- bot/commands/wish.js
+++ bot/commands/wish.js
@@ -1,7 +1,16 @@
 const { Command } = require("discord-bot");
-const { logMessage } = require("../utils");
+const utils = require("../utils");
 const db = require("../../models");
 
 module.exports = new Command("wish", function(message, args) {
-
+    let query = args.slice(1).join(" ").trim().toLowerCase();
+    utils.logMessage(message);
+    db.alias.findOne({
+        where: {
+            $and: Sequelize.where(Sequelize.fn("lower", Sequelize.col("name")), query)
+        },
+        include: db.unit
+    })
+        .then(data => utils.sendVerbose(message.channel, JSON.stringify))
+        .catch(console.error);
 });
