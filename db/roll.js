bd7a53250459dac33706fc82136dced1e868ce77
ducktrshessami <jwee761@gmail.com>
2021-02-01 18:23:57 -0800

    roll only returns relevant info

--- db/roll.js
+++ db/roll.js
@@ -3,8 +3,18 @@ const db = require("../models");
 module.exports = function(n = 1, type) {
     return db.unit.findAll({
         order: db.sequelize.random(),
+        attributes: ["type"],
         limit: n,
         where: type ? { type: type } : undefined,
-        include: [db.alias, db.image]
+        include: [
+            {
+                model: db.alias,
+                attributes: ["name", "primary", "spoiler"]
+            },
+            {
+                model: db.image,
+                attributes: ["url"]
+            }
+        ]
     })
 };
