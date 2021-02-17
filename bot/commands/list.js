05db87c572986611371b6c6dfc61ebfac7b4b6d7
ducktrshessami <jwee761@gmail.com>
2021-02-17 13:21:24 -0800

    handle claim finding in list

--- bot/commands/list.js
+++ bot/commands/list.js
@@ -1,8 +1,33 @@
 const { Command } = require("discord-bot");
 const db = require("../../models");
 
-module.exports = new Command("list", function(message, args) {
+function getUnits(player, guild) {
+    return db.guild.findOne({
+        attributes: ["id"],
+        where: { snowflake: guild }
+    })
+        .then(guildData => guildData ? db.player.findOne({
+            where: { snowflake: player },
+            include: {
+                model: db.claim,
+                where: { guildId: guildData.id },
+                include: {
+                    model: db.alias,
+                    include: db.unit
+                },
+                order: ["createdAt", "asc"]
+            }
+        }) : null)
+        .then(player => player ? player.claims : null);
+}
 
+module.exports = new Command("list", function(message, args) {
+    let snowflake = message.author.id;
+    if (args[1] && args[1] === args[1].match(/<@[0-9]+>|<@\![0-9]+>/)[0]) {
+        snowflake = args[1].match(/[0-9]+/)[0];
+    }
+    getUnits(snowflake, message.guild.id)
+        .catch(console.error);
 }, {
-    
+
 });
