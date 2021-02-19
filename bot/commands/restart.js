ee123d2f0cf5d8b2e8faef2fe6246654e95217c0
ducktrshessami <jwee761@gmail.com>
2021-02-18 19:58:13 -0800

    deleted developer restart command

--- bot/commands/restart.js
+++
@@ -1,8 +0,0 @@
-const { Command } = require("discord-bot");
-const { sendVerbose } = require("../utils");
-
-module.exports = new Command("restart", function (message) {
-    sendVerbose(message.channel, "Restarting")
-        .then(() => this.client.destroy())
-        .catch(() => this.client.destroy());
-}, { admin: true });
