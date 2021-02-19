b8ddcfdc0ac91af4106adafc90ea05291ca562f5
ducktrshessami <jwee761@gmail.com>
2021-02-18 19:51:02 -0800

    removed dotenv usage from bot

--- bot/index.js
+++ bot/index.js
@@ -4,11 +4,6 @@ const commands = require("./commands");
 
 var client;
 
-// Use defined environment vars for token and developer ID
-config.token = process.env.BOT_TOKEN;
-config.admin.push(process.env.DUCK_ID);
-config.admin.push(process.env.TILL_ID);
-
 client = new DiscordBot(config, commands);
 
 client.on("ready", function() {
